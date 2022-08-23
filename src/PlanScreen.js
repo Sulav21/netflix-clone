a5fed2401dbc3b63515e879bddabc5bfbd0faef0
Sulav Adhikari <sulavadhikari17@gmail.com>
2022-08-24 08:27:06 +1000

    deployed version

--- src/PlanScreen.js
+++ src/PlanScreen.js
@@ -1,9 +1,27 @@
 import { object } from "prop-types";
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { db } from "./firebaseAuth";
 import "./planScreen.css";
+import {loadStripe} from '@stripe/stripe-js'
 export const PlanScreen = () => {
   const [products, setProducts] = useState([]);
+  const { user } = useSelector((state) => state.user);
+  const [subscription, setSubscription] = useState(null)
+  useEffect(() => {
+    db.collection('customers')
+    .doc(user.uid)
+    .collection('subscription')
+    .get()
+    .then(querySnapshot=>{
+      querySnapshot.forEach(async subscription=>{
+        setSubscription({
+          role:subscription.data
+        })
+      })
+    })
+  }, [])
+  
   useEffect(() => {
     db.collection("products")
       .where("active", "==", true)
@@ -25,7 +43,27 @@ export const PlanScreen = () => {
   }, []);
   console.log(products);
 
-  const loadCheckout = async (price) => {};
+  const loadCheckout = async (priceId) => {
+    const docRef = await db
+      .collection("customers")
+      .doc(user.uid)
+      .collection("checkout_sessions")
+      .add({
+        price: priceId,
+        success_url:window.location.origin,
+        cancel_url:window.location.origin,
+      });
+      docRef.onSnapshot(async(snap)=>{
+        const {error, sessionId} = snap.data()
+        if(error){
+          alert(`An error occurred ${error.message}`)
+        }
+        if(sessionId){
+          const stripe = await loadStripe('pk_test_51LUNXwGzLTxHB4nfNxrmgMNUAxam6Q6ZOsnmf10CZbtktKvccrZvEPX89lbAA5JpL8DpoA83iPRrE95hQyNgaCoq009fKOlvWW')
+          stripe.redirectToCheckout({sessionId})
+        }
+      })
+  };
   return (
     <div className="planScreen">
       {Object.entries(products).map(([productId, productData]) => {
--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from "react";
 import "./row.css";
 import axios from "axios";
+import YouTube from "react-youtube";
+import movieTrailer from 'movie-trailer'
 
 export const Row = ({ title, fetchUrl, isLargeRow=false }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('')
+  const opts={
+    height:'390',
+    width:'100%',
+    playerVars:{
+      autoplay:1,
+    },
+  }
+
+  const handleClick=(movie)=>{
+    if(trailerUrl){
+    setTrailerUrl('')
+    }else{
+      movieTrailer(movie?.name || "")
+      .then(url=>{
+      const urlParams = new URLSearchParams(URL(url).search)
+      setTrailerUrl(urlParams.get('v') )
+      }).catch((error)=>console.log(error))
+    }
+      }
+  
   const baseURL = "https://image.tmdb.org/t/p/original/";
   useEffect(() => {
     const fetchMovies = async () => {
@@ -24,6 +47,7 @@ export const Row = ({ title, fetchUrl, isLargeRow=false }) => {
             ((isLargeRow && movie.poster_path) ||
             (!isLargeRow && movie.backdrop_path)) && (
               <img
+              onClick={()=>handleClick(movie)}
                 className={`row_poster ${isLargeRow && "row_posterLarge"}`}
                 key={movie.id}
                 src={
@@ -34,6 +58,8 @@ export const Row = ({ title, fetchUrl, isLargeRow=false }) => {
             )
         )}
       </div>
+     {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />} 
+      
     </div>
   );
 };
